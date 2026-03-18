28455ee28e38ba522444dccdd7d0175a0aee4388
khushi-kushwaha <khushikushwaha3009@gmail.com>
2026-03-18 20:49:50 +0530

    Initial frontend setup

---
+++ src/Components/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+
+
+const Home = () => {
+  return (
+    <div>
+        
+        
+
+    
+    </div>
+    
+  )
+}
+
+export default Home
